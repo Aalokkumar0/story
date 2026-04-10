c5d6b57511e8a3fe8456f7d2480b80c357a04a05
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-04-10 11:17:45 +0530

    Refactor ListCard widget layout and fix text alignment

--- lib/widget/list_card.dart
+++ lib/widget/list_card.dart
@@ -6,35 +6,38 @@ class ListCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      margin: EdgeInsets.only(top: 20, left: 10, right: 10),
+      margin: EdgeInsets.only(top: 10, left: 10, right: 10),
       color: Colors.amber,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Title",
-            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 23),
-          ),
-          SizedBox(height: 10),
-          Text(
-            "Contant",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 18,
-              color: Colors.blueGrey,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Title",
+              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 23),
             ),
-          ),
-          SizedBox(height: 10),
-          Text(
-            
-            "22/04/2026",
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 10,
-              color: Colors.blueGrey,
+            SizedBox(height: 10),
+            Text(
+              "Contant",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 18,
+                color: Colors.blueGrey,
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Text(
+              
+              "22/04/2026",
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 10,
+                color: Colors.blueGrey,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
