381b659aaf7ff83a5a6bb7418c2e872abc01a1b6
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-04-10 11:15:01 +0530

    Refactor ListCard widget layout and improve text formatting

--- lib/widget/list_card.dart
+++ lib/widget/list_card.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
-
-class ListCard extends StatelessWidget{
+class ListCard extends StatelessWidget {
   const ListCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      
+      margin: EdgeInsets.only(top: 20, left: 10, right: 10),
       color: Colors.amber,
-      child: Padding(
-        padding:EdgeInsets.all(18.0),
-        child: Text("Title "),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Title",
+            style: TextStyle(fontWeight: FontWeight.w900, fontSize: 23),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Contant",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 18,
+              color: Colors.blueGrey,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            
+            "22/04/2026",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              fontSize: 10,
+              color: Colors.blueGrey,
+            ),
+          ),
+        ],
       ),
     );
   }
-  
 }
