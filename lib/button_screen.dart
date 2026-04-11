04744d2c88af808c93a237749332816de69afd91
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-04-11 14:52:55 +0530

    Enhance ButtonScreen by adding controllers for title and content fields, and update TextField decorations

--- lib/button_screen.dart
+++ lib/button_screen.dart
@@ -8,21 +8,70 @@ class ButtonScreen extends StatefulWidget {
 }
 
 class _ButtonScreenState extends State<ButtonScreen> {
+
+
+  final _titleEditingController = TextEditingController();
+
+   final _contentEditingController = TextEditingController();
+
+  void printtitle() {
+    // ignore: avoid_print
+    print(_titleEditingController.text);
+    // ignore: avoid_print
+    print(_contentEditingController.text);
+
+    _titleEditingController.clear();
+    _contentEditingController.clear();
+
+
+  }
+
+ 
+  
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
         padding: EdgeInsets.all(40),
-        
-        child:Column(
+
+        child: Column(
           children: [
             TextField(
+              controller: _titleEditingController,
               decoration: InputDecoration(
                 icon: Icon(Icons.send),
-                hintText: "Enter Title...",
-                
-                border: OutlineInputBorder()),
+                labelText: "Enter Title...",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+            ),
+            
+            SizedBox(height: 20,),
+
+
+            TextField(
+              controller: _contentEditingController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.send),
+                labelText:"Contant...",
+
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20)
+              )
+              ),
+            ),
+
+
+            SizedBox(height: 20,),
+
+            ElevatedButton(
+              onPressed: () {
+                printtitle();
+              },
+              child: Text("print"),
             ),
           ],
         ),
